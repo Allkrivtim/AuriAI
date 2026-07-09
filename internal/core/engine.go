3c7280638a164ad0bcd205aa51f1d9f9d80cf211
Allkrivtim <feesta009@gmail.com>
2026-07-10 01:43:49 +0300

    v0.0.0.3 Complete core v1

--- internal/core/engine.go
+++ internal/core/engine.go
@@ -13,7 +13,26 @@ func NewEngine(llm LLM, store Store) Engine {
 	return &engine{llm: llm, store: store}
 }
 
-func (e *engine) Handle(ctx context.Context, message InboundMessage) (OutboundMessage, error) {
+func (e *engine) Handle(ctx context.Context, inmessage InboundMessage) (OutboundMessage, error) {
+	message := Message{Role: RoleUser, Text: inmessage.Text}
+	//Store message
+	err := e.store.AppendMessage(ctx, inmessage.SessionID, message)
+	if err != nil {
+		return OutboundMessage{}, err
+	}
 
-	return OutboundMessage{}, nil
+	//Get history
+	history, err := e.store.History(ctx, inmessage.SessionID, 50)
+	if err != nil {
+		return OutboundMessage{}, err
+	}
+
+	//Create response to LLM provider
+	resp, err := e.llm.Complete(ctx, CompletionRequest{System: "", Messages: history})
+	if err != nil {
+		return OutboundMessage{}, err
+	}
+
+	//Return responce
+	return OutboundMessage{resp.Text, inmessage.SessionID}, nil
 }
--- internal/core/message.go
+++
@@ -1,8 +0,0 @@
-package core
-
-import "AssistantAI/internal/adapters/llm/openrouter"
-
-func AskAI(message string) (string, error) {
-	resp, err := openrouter.MessageAI(message)
-	return resp, err
-}
